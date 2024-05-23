d1e12314b6688c4ede8643820c36f86fd37ae47b
AryaShakya1 <arya@ramailo.tech>
2024-05-23 22:56:02 +0545

    Add navItems and navLogo to the Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
 import tailwindLogo from '../assets/tailwind.svg'
+import NavItem from './NavItem'
+import NavLogo from './NavLogo'
+import GithubLogo from '../assets/github-mark-white.svg'
+import DarkModeLogo from '../assets/dark-mode.svg'
 
 function Navbar() {
     return (
-        <div className='flex flex-row justify-between'>
-            <img src={tailwindLogo} alt="Tailwind Logo" width={300} height={300}/>
-            <div>Navbar</div>
+        <div className='flex flex-row justify-between px-4'>
+            <img src={tailwindLogo} alt="Tailwind Logo" width={200} height={300} />
+            <ul className='flex flex-row p-4 mt-0 items-center'>
+                <NavItem text='Docs' />
+                <NavItem text='Components' />
+                <NavItem text='Blog' />
+                <NavItem text='Showcase' />
+                <NavLogo src={DarkModeLogo} href='' />
+                <NavLogo src={GithubLogo} href='https://github.com/tailwindlabs/tailwindcss' />
+            </ul>
         </div>
     )
 }
