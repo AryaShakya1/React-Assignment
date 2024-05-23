dde8fae774b4bdf89bed61028f2f0793fedf7ed0
AryaShakya1 <arya@ramailo.tech>
2024-05-23 21:53:34 +0545

    add background color

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,10 @@ import Navbar from '../components/Navbar'
 
 function HomePage() {
     return (
-        <Navbar />
+        <div className='flex flex-col bg-[#0A1322] min-h-screen'>
+            <Navbar />
+            <div></div>
+        </div>
     )
 }
 
