1891f1989c94103b6bb8c45ee6befa0e46bf4fe3
AryaShakya1 <arya@ramailo.tech>
2024-05-24 13:05:49 +0545

    Change padding

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeroSection({ primaryText, secondaryText }: { primaryText: string, secondaryText: string }) {
     return (
-        <div className='flex flex-col items-center justify-center p-8 m-8 '>
+        <div className='flex flex-col items-center justify-center p-4 '>
             <p className='text-white text-6xl font-extrabold text-center p-6 max-w-6xl'>{primaryText}</p>
             <p className='text-white text-xl text-center p-4 max-w-[52rem]' >{secondaryText}</p>
         </div>
