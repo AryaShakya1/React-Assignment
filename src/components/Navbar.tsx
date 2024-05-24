d8dcb8aa4c3cd603c0744b33a4a7b5228442fb8d
AryaShakya1 <arya@ramailo.tech>
2024-05-24 15:45:37 +0545

    Add dark mode toggle functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,36 @@
 import tailwindLogo from '../assets/tailwind.svg'
+import lightTailwindLogo from '../assets/tailwind-light.svg'
 import NavItem from './NavItem'
 import NavLogo from './NavLogo'
 import GithubLogo from '../assets/github-mark-white.svg'
+import lightGithubLogo from '../assets/github-mark.svg'
 import DarkModeLogo from '../assets/dark-mode.svg'
+import { useState } from "react";
+import LightModeLogo from '../assets/light-mode.svg'
 
 function Navbar() {
+    const [isDark, setIsDark] = useState(false)
+    const handleToggle = () => {
+        setIsDark(!isDark);
+        if (!isDark) {
+            document.body.classList.add('dark');
+        } else {
+            document.body.classList.remove('dark');
+        }
+    }
     return (
         <div className='flex flex-row justify-between px-4'>
-            <img src={tailwindLogo} alt="Tailwind Logo" width={200} height={300} />
+            <img src={isDark ? tailwindLogo : lightTailwindLogo} alt="Tailwind Logo" width={200} height={300} />
             <ul className='flex flex-row p-4 mt-0 items-center'>
                 <NavItem text='Docs' />
                 <NavItem text='Components' />
                 <NavItem text='Blog' />
                 <NavItem text='Showcase' />
-                <NavLogo src={DarkModeLogo} href='' />
-                <NavLogo src={GithubLogo} href='https://github.com/tailwindlabs/tailwindcss' />
+
+                <button onClick={handleToggle}>
+                    <NavLogo src={isDark ? DarkModeLogo : LightModeLogo} />
+                </button>
+                <NavLogo src={isDark ? GithubLogo : lightGithubLogo} />
             </ul>
         </div>
     )
